7c8deb2c18b72ce85098188713837ca0570466ba
Shane Creighton-Young <srcreigh@uwaterloo.ca>
2014-08-09 02:28:26 -0400

    Added more documentation

--- lib/updates.py
+++ lib/updates.py
@@ -9,12 +9,17 @@ import requests
 import re
 from termcolor import colored
 
+# The narrative in the comments for this code is as follows:
+# 'the user' is a user of our app, who we are sending notifications to
+# 'the/an admin' is a user of this script
+
 # Environment variables and URLs
 FIREBASE_SECRET = os.getenv('FIREBASE_SECRET', None)
 FIREBASE_URL = os.getenv('FIREBASE_URL', None)
 GCM_API_KEY = os.getenv('GCM_API_KEY', None)
 GCM_URL = 'https://android.googleapis.com/gcm/send'
 
+# I/O helper functions
 def printError(msg):
    print "[ %s ] %s" % (colored('ERROR', 'red'), msg)
 
@@ -57,8 +62,8 @@ def checkEnvVars():
       return False
 
    # Check that the firebase secret is valid. Note that foo.firebaseio.com/test.json 
-   # is a valid that doesn't have public read permission, so we should only be able
-   # to read it if the secret is valid.
+   # is a valid data path that doesn't have public read permission, so we should only 
+   # be able to read it if the secret is valid.
    printInfo('Checking that the Firebase secret is valid...')
    r = requests.get("%s/test.json" % FIREBASE_URL, params = { 'auth': FIREBASE_SECRET })
    if 'error' in r.json():
@@ -69,7 +74,8 @@ def checkEnvVars():
        return False
    printInfo('Firebase secret OK')
 
-   # Check that the GCM API key is valid.
+   # This sends a dummy GCM request that won't actually do anything meaningful. However,
+   # it will give an error if the API key is invalid for some reason.
    printInfo('Checking that the GCM API key is valid...')
    kwargs = { 
          'headers': { 
@@ -93,8 +99,6 @@ def checkEnvVars():
    # If we get here, then our environment variables are all good. :+1:
    return True
 
-
-
 # Returns a dict of the new data on success, or None if failed.
 def pushToFirebase():
 
@@ -113,6 +117,8 @@ def pushToFirebase():
    print ''
    printInfo('Confirm update:')
    print json.dumps(new_data, indent=2)
+
+   # Keep taking input until the admin write something that resembles yes or no
    while True:
        confirm = raw_input(make_prompt('yn')).lower()
        if confirm[0] == 'y' or confirm[0] == 'n':
@@ -122,7 +128,8 @@ def pushToFirebase():
        printInfo('Cancelled')
        return None
 
-   # Note: Confirm must be 'y...' at this point.
+   # Note: confirm[0] must be 'y...' at this point, so we can assume the admin
+   # has said "yes this data is good"
 
    # Add the update to firebase.
    r = requests.post('%s/updates.json' % FIREBASE_URL,
@@ -139,6 +146,18 @@ def pushToFirebase():
 
 # Takes a dict of data that corresponds to the data that was just added to 
 # Firebase, and sends a notification to all our users about the new update.
+#
+# TODO: The GCM payload is max 4kb, and we currently aren't doing any checking
+# on the size of the payload, and if GCM fails then, by the way this code is 
+# structured, we won't be able to send users notifications for this update.
+#
+# TODO: Can probably add functionality here to send a notification for the 
+# most recent update in /updates.json, and have another admin script that uses
+# it in the case that GCM fails for some reason and we didn't send the users
+# notifications.
+#
+# TODO: We can only send 1000 registration_ids at a time to GCM. To be safe we should
+# probably check for this and use a loop for multiple requests.
 def pingGCM(data):
 
    # Get all the android registration ids from Firebase
@@ -188,9 +207,17 @@ def pingGCM(data):
       printError('GCM error: %s' % r.text)
       return None
 
+   # See [1] for a reference on the result values of these kinds of GCM requests.
+   # Thankfully they're pretty sane.
+   # [1]: https://developer.android.com/google/gcm/http.html#example-responses
+
+   # r.json()['success'] and failure report the number of successful and failed
+   # notifications, respectively.
    printInfo('%d notifications sent successfully.' % r.json()['success'])
    printInfo('%d notifications failed.' % r.json()['failure'])
 
+   # When we send off a GCM request, sometimes GCM will have something to say about
+   # the registration IDs we tried to send a message to.
    print ''
    printInfo('Performing bookkeeping...')
    results = r.json()['results']
@@ -200,13 +227,18 @@ def pingGCM(data):
       result = results[i]
 
       if 'error' in result:
+         # GCM will give us this result if we give it a) a malformed registration ID,
+         # or b) an ID that corresponds to a user who has uninstalled our app. In both cases,
+         # we should delete the registration ID from firebase.
          if result['error'] == 'Unregistered' or result['error'] == 'InvalidRegistration':
 
-            # Delete the registration ID from firebase
+            # Delete the registration ID from firebase.
             printInfo('Registration ID \'%s\' is invalid; deleting from Firebase...' % registration_id)
             r = requests.delete('%s/notifications/android/%s.json' % (FIREBASE_URL, registration_id),
                   params = { 'auth': FIREBASE_SECRET })
             
+      # Sometimes GCM will be like 'yo! you gave me this registration ID, but I have a new one for this
+      # user and in the future you should use it instead'.
       elif 'registration_id' in result:
 
          new_regid = result['registration_id']
@@ -217,18 +249,3 @@ def pingGCM(data):
                params = { 'auth': FIREBASE_SECRET })
          r = requests.put('%s/notifications/android/%s.json' % new_regid,
                params = { 'auth': FIREBASE_SECRET })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
